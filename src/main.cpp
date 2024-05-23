effcda88a3103137e24bf4ff40fbe62a2f914964
Dushan18095 <civorodotnasud@gmail.com>
2024-05-24 01:32:57 +0200

    Ubacen nearest neighbor i algoritam sa dinamickim programiranjem

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <cmath>
+
 using namespace std;
 
 int findWeigth(vector<int> node1, vector<int> node2) // Funkcija za izracunavanje tezine veze izmedju argumenta node1 i node2
@@ -66,7 +68,7 @@ void printPermutations(vector<vector<int>> permutations) // Stampa sve elemenent
 	{
 		for (unsigned int j = 0; j < permutations[0].size(); j++)
 		{
-			cout << char(permutations[i][j]+65);
+			cout << char(permutations[i][j] + 65);
 		}
 		cout << endl;
 	}
@@ -77,54 +79,58 @@ int factorial(int n) // Vraca faktorijel broja
 	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
 
-vector<int> tspBruteForce(vector<vector<int>> graph)
+vector<int> nearestNeighbor(const vector<vector<int>> graph) // Aproksimativni algoritam za resavanje TSP
 {
-    int n = graph.size();
-    vector<int> vertex;
-    vector<int> path;
-    for (int i = 1; i < n; i++)
-        vertex.push_back(i);
-    int min_cost = INT_MAX;
-    while(next_permutation(vertex.begin(), vertex.end()))
-     {
-        int current_cost = 0;
-        int j = 0;
-        for (int i = 0; i < vertex.size(); i++) {
-            current_cost += graph[j][vertex[i]];
-            j = vertex[i];
-        }
-        current_cost += graph[j][0];
-        if (current_cost < min_cost)
-        {
-            min_cost = min(min_cost, current_cost);
-            path = vertex;
-        }
+	int n = graph.size();
+	vector<int> path;
+	vector<bool> visited(n, false);
+
+	path.push_back(0);
+	visited[0] = true;
+	int currentCity = 0;
+
+	for (int i = 1; i < n; ++i)
+	{
+		int minDist = INT_MAX;
+		int nextCity = -1;
+
+		for (int j = 0; j < n; ++j)
+		{
+			if (!visited[j] && graph[currentCity][j] < minDist)
+			{
+				minDist = graph[currentCity][j];
+				nextCity = j;
+			}
+		}
+
+		path.push_back(nextCity);
+		visited[nextCity] = true;
+		currentCity = nextCity;
 	}
-	path.insert(path.begin(), 0);
-    return path;
+
+	return path;
 }
 
-vector<int> mergePerms(vector<int> perm1, vector<int> perm2)
-{	
-	if(perm1.size() == 0)
+vector<int> mergePerms(vector<int> perm1, vector<int> perm2) // Funkcija za spajanje dva niza sa uklanjanjem prekpalanja
+{
+	if (perm1.size() == 0)
 		return perm2;
-	if(perm2.size() == 0)
+	if (perm2.size() == 0)
 		return perm1;
 	vector<int> result = perm1;
-    
-	int n;
-	int m;
-	for (int i = 0; i < perm2.size(); i++)
+
+	unsigned int n;
+	for (unsigned int i = 0; i < perm2.size(); i++)
 	{
 		n = 0;
-		for (int j = 0; j < perm2.size()-i; j++)
+		for (unsigned int j = 0; j < perm2.size() - i; j++)
 		{
-			if(perm1[perm1.size()-perm2.size()+i+j] == perm2[j])
+			if (perm1[perm1.size() - perm2.size() + i + j] == perm2[j])
 			{
 				n++;
 			}
 		}
-		if(n == perm2.size()-i)
+		if (n == perm2.size() - i)
 		{
 			vector<int>::iterator it = perm2.begin();
 			advance(it, n);
@@ -132,8 +138,62 @@ vector<int> mergePerms(vector<int> perm1, vector<int> perm2)
 			return result;
 		}
 	}
-	//set_union(perm1.begin(), perm1.end(), perm2.begin(), perm2.end(), back_inserter(result));
-    return result;
+	return result;
+}
+
+vector<int> tspDynamicProgramming(vector<vector<int>> graph) // Egzaktni algoritam za resavanje TSP
+{
+	int n = graph.size();
+	vector<vector<int>> dp(1 << n, vector<int>(n, INT_MAX)); // DP tabela
+	vector<vector<int>> parent(1 << n, vector<int>(n, -1));	 // Tabela roditelja
+	dp[1][0] = 0;											 // Početni čvor
+
+	for (int mask = 1; mask < (1 << n); mask++)
+	{
+		for (int u = 0; u < n; u++)
+		{
+			if (mask & (1 << u))
+			{ // Provera da li je čvor u skupu mask
+				for (int v = 0; v < n; v++)
+				{
+					if (!(mask & (1 << v)))
+					{ // Provera da li je čvor v van skupa mask
+						if (dp[mask | (1 << v)][v] > dp[mask][u] + graph[u][v])
+						{
+							dp[mask | (1 << v)][v] = dp[mask][u] + graph[u][v];
+							parent[mask | (1 << v)][v] = u; // Pamćenje roditelja
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Pronalaženje optimalne putanje
+	int min_cost = INT_MAX;
+	int last_node = -1;
+	for (int i = 0; i < n; i++)
+	{
+		if (min_cost > dp[(1 << n) - 1][i])
+		{
+			min_cost = dp[(1 << n) - 1][i];
+			last_node = i;
+		}
+	}
+
+	// Rekonstrukcija putanje
+	vector<int> path;
+	int mask = (1 << n) - 1;
+	while (last_node != -1)
+	{
+		path.push_back(last_node);
+		int new_last_node = parent[mask][last_node];
+		mask ^= (1 << last_node); // Brisanje poslednjeg čvora iz skupa mask
+		last_node = new_last_node;
+	}
+	reverse(path.begin(), path.end()); // Rezultat se mora invertovati jer smo radili unazad
+
+	return path;
 }
 
 int main()
@@ -157,28 +217,27 @@ int main()
 	printPermutations(permutations); // Stampanje svih permtutacija
 
 	vector<vector<int>> graph(connectGraph(permutations)); // Pravljenje matrice povezanosti grafa
-	for(int i = 0; i < graph.size(); i++)
+	for (unsigned int i = 0; i < graph.size(); i++)
 	{
 		graph[i][0] = 0;
 	}
-	printAdjMatrix(graph); // Stampanje matrice povezanosti
+	//printAdjMatrix(graph); // Stampanje matrice povezanosti
 
-    vector<int> pathIndex = tspBruteForce(graph);
-    cout << "Shortest path by index:" << endl;
-    for(int i = 0; i < pathIndex.size(); i++)
-    {
-        cout << pathIndex[i] << endl;
-    }
-    
-	
-    vector<int> superpermIndex;
-    for (unsigned int i = 0; i < pathIndex.size(); i++)
-    {
-		superpermIndex = mergePerms(superpermIndex, permutations[pathIndex[i]]);
-    }
-	for(int num : superpermIndex)
+	vector<int> pathIndex = nearestNeighbor(graph); // Koristi nearestNeighbor umesto tspBruteForce
+	cout << "Shortest path by index:" << endl;
+	for (int i = 0; i < pathIndex.size(); i++)
 	{
-		cout << char(num+65);
+		cout << pathIndex[i] << endl;
+	}
+
+	vector<int> superpermIndex;
+	for (unsigned int i = 0; i < pathIndex.size(); i++)
+	{
+		superpermIndex = mergePerms(superpermIndex, permutations[pathIndex[i]]);
+	}
+	for (int num : superpermIndex)
+	{
+		cout << num+1;
 	}
 	return 0;
 }
